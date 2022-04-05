2e5ce4711be4c23fe47e522e550750d4ddb5ade8
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-05 11:30:45 +0530

    Added to test the last name.

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,6 +1,11 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Author:-Ashvini Kanojia
+ * Date:-04/04/2022
+ */
+
 public class UserRegistration {
     /*
      * @purpose: As a User need to enter a valid First Name. First name starts with
@@ -16,6 +21,23 @@ public class UserRegistration {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+    /*
+     * @purpose: As a User need to enter a valid last Name. Last name starts with
+     * Cap and has minimum 3 characters
+     *
+     * @param: lastName
+     *
+     * @function: To check last name is Valid or not
+     *
+     * @returns true or false
+     */
+    public boolean lastName(String lastName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
         return matcher.matches();
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -6,13 +6,26 @@ public class UserRegistrationTest {
 
     @Test
     public void givenFirstName_WhenValid_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Rutuja");
+        boolean result = userRegistration.firstName("Ashvini");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFirstName_WhenInvalid_ShouldReturnFalse() {
-        boolean result = userRegistration.firstName("rutuja");
+        boolean result = userRegistration.firstName("ashvini");
         Assert.assertEquals(false, result);
     }
+
+    @Test
+    public void givenLastName_WhenValid_ShouldReturnTrue() {
+        boolean result = userRegistration.lastName("Kanojia");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenInvalid_ShouldReturnFalse() {
+        boolean result = userRegistration.lastName("kanojia");
+        Assert.assertEquals(false, result);
+    }
+
 }
