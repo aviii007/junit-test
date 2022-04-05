bbde0e4063cf0d246738e5640ddbe565a3ac11f1
Ashvini Kanojia <99378217+aviii007@users.noreply.github.com>
2022-04-05 11:39:19 +0530

    Update UserRegistrationTest.java

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -6,25 +6,25 @@ public class UserRegistrationTest {
 
     @Test
     public void givenFirstName_WhenValid_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Rutuja");
+        boolean result = userRegistration.firstName("Ashvini");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFirstName_WhenInvalid_ShouldReturnFalse() {
-        boolean result = userRegistration.firstName("rutuja");
+        boolean result = userRegistration.firstName("ashvini");
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void givenLastName_WhenValid_ShouldReturnTrue() {
-        boolean result = userRegistration.lastName("Nandiwadekar");
+        boolean result = userRegistration.lastName("kanojia");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenLastName_WhenInvalid_ShouldReturnFalse() {
-        boolean result = userRegistration.lastName("patil");
+        boolean result = userRegistration.lastName("kanojia");
         Assert.assertEquals(false, result);
     }
 
